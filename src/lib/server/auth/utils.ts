ca4de4040d1630ccf2ff6078feb8a94900562593
alongcamewayne <dubdotworth@gmail.com>
2024-01-07 17:19:00 -0600

    refactor

--- src/lib/server/auth/utils.ts
+++ src/lib/server/auth/utils.ts
@@ -1,11 +1,53 @@
 import type { Cookies } from '@sveltejs/kit';
+import { Argon2id } from 'oslo/password';
 import { lucia } from '$lib/server/auth';
+import { z } from 'zod';
+import { db } from '$lib/server/db';
+import { users } from '$lib/server/db/schema';
 import { createId } from '$lib/utils';
+
+type CreateUserArgs = {
+	username: string;
+	password: string;
+};
+
+type CreateUserResult =
+	| { success: true; data: { id: string; username: string } }
+	| { success: false; error: string };
 
 type CreateUserSessionArgs = {
 	userId: string;
 	cookies: Cookies;
 };
+
+export async function createUser({
+	username,
+	password,
+}: CreateUserArgs): Promise<CreateUserResult> {
+	try {
+		const result = await z
+			.object({
+				id: z.string().startsWith('user_').optional().default(createId('user')),
+				username: z.string().min(3),
+				password: z
+					.string()
+					.min(7)
+					.transform(async (pw) => await new Argon2id().hash(pw)),
+			})
+			.parseAsync({ username, password });
+
+		await db.insert(users).values(result);
+
+		return {
+			success: true,
+			data: { id: result.id, username: result.username },
+		};
+	} catch (error) {
+		// todo: handle errors better
+		console.log(error);
+		return { success: false, error: String(error) };
+	}
+}
 
 export async function createUserSession({
 	userId,
