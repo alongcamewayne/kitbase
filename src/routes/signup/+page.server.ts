ad7e6560a255aba1c717cadc46fc3d643a585d31
Wayne Worth <121204048+alongcamewayne@users.noreply.github.com>
2024-01-07 17:19:42 -0600

    auth refactor (#3)

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -1,18 +1,6 @@
 import type { Actions, PageServerLoad } from './$types';
-import type { SqliteError } from 'better-sqlite3';
 import { fail, redirect } from '@sveltejs/kit';
-import { z } from 'zod';
-import { Argon2id } from 'oslo/password';
-import { db } from '$lib/server/db';
-import { users } from '$lib/server/db/schema';
-import { createUserSession } from '$lib/server/auth/utils';
-import { createId } from '$lib/utils';
-
-// todo: use drizzle-zod
-const form = z.object({
-	username: z.string().min(3),
-	password: z.string().min(7),
-});
+import { createUser, createUserSession } from '$lib/server/auth/utils';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (locals.user) throw redirect(302, '/');
@@ -22,34 +10,16 @@ export const load: PageServerLoad = async ({ locals }) => {
 export const actions: Actions = {
 	default: async ({ cookies, request }) => {
 		const formData = await request.formData();
-		const result = form.safeParse({
-			username: formData.get('username'),
-			password: formData.get('password'),
-		});
+		const username = String(formData.get('username') || '');
+		const password = String(formData.get('password') || '');
 
-		if (!result.success) {
-			return fail(400, { message: result.error.errors[0].message });
+		const result = await createUser({ username, password });
+
+		if (result.success) {
+			await createUserSession({ userId: result.data.id, cookies });
+			return redirect(302, '/');
 		}
 
-		const { username, password: unhashedPassword } = result.data;
-		const userId = createId('user');
-		const password = await new Argon2id().hash(unhashedPassword);
-
-		try {
-			await db.insert(users).values({ id: userId, username, password });
-		} catch (error: unknown) {
-			const sqliteError = error as SqliteError;
-			if (
-				'code' in sqliteError &&
-				sqliteError.code === 'SQLITE_CONSTRAINT_UNIQUE'
-			) {
-				return fail(400, { message: 'username taken' });
-			}
-			console.log(error);
-			return fail(400, { message: 'something went wrong' });
-		}
-
-		await createUserSession({ userId, cookies });
-		return redirect(302, '/');
+		return fail(400, { message: result.error });
 	},
 };
